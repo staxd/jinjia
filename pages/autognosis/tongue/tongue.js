0dde9e76bf8e2da56586df858c123d1e73b32fed
吴有章 <1076355993@qq.com>
2019-01-18 17:53:13 +0800

    11

--- pages/autognosis/tongue/tongue.js
+++ pages/autognosis/tongue/tongue.js
@@ -448,7 +448,7 @@ Page({
     var shareObj = {
       title: "金荚中医",        // 默认是小程序的名称(可以写slogan等)
       path: '/pages/index/index?inviteicode=' + inviteicode,        // 默认是当前页面，必须是以‘/’开头的完整路径
-      imageUrl: '/images/indexPic.png',     //自定义图片路径，可以是本地文件路径、代码包文件路径或者网络图片路径，支持PNG及JPG，不传入 imageUrl 则使用默认截图。显示图片长宽比是 5:4
+      imageUrl: '/images/indexPic.jpg',     //自定义图片路径，可以是本地文件路径、代码包文件路径或者网络图片路径，支持PNG及JPG，不传入 imageUrl 则使用默认截图。显示图片长宽比是 5:4
       success: function (res) {
         // 转发成功之后的回调
         if (res.errMsg == 'shareAppMessage:ok') {
